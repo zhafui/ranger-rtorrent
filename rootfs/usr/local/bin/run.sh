513051e704c87b3b01ddd9b99750c2cf190cd43a
zhafui <zhafui@vorlon.net.nz>
2021-09-04 20:35:48 +1200

    added run.sh back to original

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+mkdir -p /data/torrents
+mkdir -p /data/.watch
+mkdir -p /data/.session
+
 rm -f /data/.session/rtorrent.lock
 
 chown -R $UID:$GID /data /home/torrent /tmp /usr/flood/dist /flood-db /etc/s6.d
@@ -8,4 +12,4 @@ if [ ${RTORRENT_SOCK} = "false" ]; then
     sed -i -e 's|^scgi_local.*$|scgi_port = 0.0.0.0:5000|' /home/torrent/.rtorrent.rc
 fi
 
-
+exec su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
